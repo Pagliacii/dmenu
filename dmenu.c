4a18e9e6be7a87e8513c10f9cfd15fe72e45baa9
Pagliacii <huangmianrui0310@outlook.com>
2020-01-05 17:59:24 +0800

    feat(patches > printinputtext): apply printinputtext patch

--- dmenu.c
+++ dmenu.c
@@ -49,6 +49,7 @@ static struct item *items = NULL;
 static struct item *matches, *matchend;
 static struct item *prev, *curr, *next, *sel;
 static int mon = -1, screen;
+static int use_text_input = 0;
 
 static Atom clip, utf8;
 static Display *dpy;
@@ -699,7 +700,10 @@ insert:
 		break;
 	case XK_Return:
 	case XK_KP_Enter:
-		puts((sel && !(ev->state & ShiftMask)) ? sel->text : text);
+		if (use_text_input)
+			puts((sel && (ev->state & ShiftMask)) ? sel->text : text);
+		else
+			puts((sel && !(ev->state & ShiftMask)) ? sel->text : text);
 		if (!(ev->state & ControlMask)) {
 			savehistory((sel && !(ev->state & ShiftMask))
 				    ? sel->text : text);
@@ -978,6 +982,8 @@ main(int argc, char *argv[])
 			instant = 1;
 		else if (!strcmp(argv[i], "-P"))   /* is the input a password */
 			passwd = 1;
+		else if (!strcmp(argv[i], "-t")) /* favors text input over selection */
+			use_text_input = 1;
 		else if (i + 1 == argc)
 			usage();
 		/* these options take one argument */
