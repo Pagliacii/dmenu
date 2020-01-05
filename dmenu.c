139159a0d516df52b784df59588e78b052714c40
Pagliacii <huangmianrui0310@outlook.com>
2020-01-05 14:54:45 +0800

    docs(dmenu.1): add option descriptions about '-F'

--- dmenu.c
+++ dmenu.c
@@ -807,7 +807,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bfinv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
+	fputs("usage: dmenu [-bcfFinv] [-l lines] [-p prompt] [-fn font] [-m monitor]\n"
 	      "             [-nb color] [-nf color] [-sb color] [-sf color] [-w windowid]\n", stderr);
 	exit(1);
 }
